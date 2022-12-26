e9b46e29db709a211bd8eca556db020642e47883
Mitch <mitchhuang777@gmail.com>
2022-12-26 18:05:28 +0800

    Update freelancer.py

--- freelancer.py
+++ freelancer.py
@@ -17,6 +17,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 # Path to the chromedriver
 # Chromedriver 108.0.5359.22
@@ -53,29 +54,16 @@ from tqdm import tqdm
 import openpyxl
 import warnings
 
-'''
-# Open the freelancer.com
-driver.get("https://www.freelancer.com/login")
-# Open a file to read the username and password
-with open("account.txt", "r") as f:
-    user = f.readline()
-    # Select the username between " and "
-    user = re.findall(r'"(.*?)"', user)[0]
-    # Select pswd between " and "
-    pswd = f.readline()
-    pswd = re.findall(r'"(.*?)"', pswd)[0]
-    '''
-
 # page source: https://www.freelancer.com/job/
 driver.get("https://www.freelancer.com/job/")
 soup = BeautifulSoup(driver.page_source, "html.parser")
 elements = soup.select('.PageJob-category-link[title]')
 
 # Create a new excel file
-df = pd.DataFrame(columns=['Title', 'Number'])
+df1 = pd.DataFrame(columns=['Title', 'Number'])
 
-
-"""
+writer = pd.ExcelWriter('freelancer.xlsx', engine='openpyxl')
+cnt = 0
 # write in tqdm
 for element in tqdm(elements):
     # Job title
@@ -91,14 +79,16 @@ for element in tqdm(elements):
         number = int(number.group(1))
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=FutureWarning)
-            df = df.append({'Title': title, 'Number': number}, ignore_index=True)      
+            new_row = pd.DataFrame({'Title': [title], 'Number': [number]})
+            df1 = pd.concat([df1, new_row], ignore_index=True)
     else:
         # If the number was not found, save title to the excel file
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=FutureWarning)
-            df = df.append({'Title': title, 'Number': np.nan}, ignore_index=True)
-    df.to_excel("freelancer.xlsx", index=False)
-"""
+            new_row = pd.DataFrame({'Title': [title], 'Number': [np.nan]})
+            df1 = pd.concat([df1, new_row], ignore_index=True)       
+    
+df1.to_excel(writer, sheet_name="Job Categories", index=False)
 
 search_element = "python"
 driver.get("https://www.freelancer.com/job-search/" + search_element + "/")
@@ -108,11 +98,6 @@ radio_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//label[contain
 driver.execute_script("arguments[0].scrollIntoView();", radio_button)
 # Click the radio button
 radio_button.click()
-
-# Close the driver
-# driver.close()
-
-# Sort by Latest
 
 # Find the checkbox element and click it
 checkbox = wait.until(EC.element_to_be_clickable((By.XPATH, "//label[contains(., 'Fixed Price Projects')]")))
@@ -124,84 +109,135 @@ checkbox.click()
 cnt = 0
 
 time.sleep(1)
-# Wait for the job listings to be present
-WebDriverWait(driver, 10).until(
-  EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.JobSearchCard-item'))
-)
 
-# Find all the job listings
-job_listings = driver.find_elements(By.CSS_SELECTOR, '.JobSearchCard-item')
 cnt = 0
 
-# Iterate through the job listings
-for job_listing in job_listings:
-    if cnt == 10:
-        break
-    # Find the title element
-    # title_element = job_listing.find_elements(By.CSS_SELECTOR, '.JobSearchCard-primary-heading-link')
-    # Get the html from the job_listing
-    # html = job_listing.get_attribute('innerHTML')
-    project_element = job_listing.find_element(By.CSS_SELECTOR, 'a.JobSearchCard-primary-heading-link')
-    project = project_element.text
-    print(project)
-    
-    days_element = job_listing.find_element(By.CSS_SELECTOR, 'span.JobSearchCard-primary-heading-days')
-    days = days_element.text
-    print(days)
-    
-    avg_price_element = job_listing.find_element(By.CSS_SELECTOR, 'div.JobSearchCard-secondary-price')
-    avg_price = avg_price_element.text
-    print(avg_price)
-    
-    bid_element = job_listing.find_element(By.CSS_SELECTOR, 'div.JobSearchCard-secondary-entry')
-    bid = bid_element.text
-    print(bid)
-    cnt += 1
-    
-    element = job_listing.find_element(By.CSS_SELECTOR, 'a.JobSearchCard-primary-heading-link')
-    url = element.get_attribute('href')
-    print(url)
-    
-    # Open a new tab, switch to it, and go to the url
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(url) 
-    
-    # wait for the QTextEdit element to be visible
-    # find the paragraph element using its class name
-    parent_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "PageProjectViewLogout-detail")))
-    paragraph_element = parent_element.find_elements(By.TAG_NAME, "p")
-    # Extract the text from each p element
-    text_list = []
-    for p_element in paragraph_element:
-        text = p_element.text
-        text_list.append(text)
-    combined_text = "\n".join(text_list)
+# Create df2
+df2 = pd.DataFrame(columns=['Project', 'Days', 'Avg Price ($)', 'Budget', 'Bid', 'URL', 'Paragraph'])
 
-    
-    budget_element = driver.find_element(By.XPATH, "//p[@class='PageProjectViewLogout-header-byLine']")
-    budget = budget_element.text
-    budget = budget[7:]
-    print(budget)
-    
-    # Save the data to the excel file
-    # project, days, avg_price, budget, bid, url, paragraph, 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=FutureWarning)
-        df = df.append({'Project': project, 'Days': days, 'Avg Price': avg_price, 'Budget': budget, 'Bid': bid, 'URL': url, 'Paragraph': combined_text}, ignore_index=True)
-    df.to_excel("freelancer.xlsx", index=False)
-    
-    # Close the tab
-    driver.close()
-    
-    # Switch back to the first tab
-    driver.switch_to.window(driver.window_handles[0])
-    
+def get_project():
+    global df2
+    # Iterate through the job listings
+    for i, job_listing in enumerate(job_listings):
+        try:
+            # Find the title element
+            # title_element = job_listing.find_elements(By.CSS_SELECTOR, '.JobSearchCard-primary-heading-link')
+            # Get the html from the job_listing
+            # html = job_listing.get_attribute('innerHTML')
+            project_element = job_listing.find_element(By.CSS_SELECTOR, 'a.JobSearchCard-primary-heading-link')
+            project = project_element.text
+            
+            days_element = job_listing.find_element(By.CSS_SELECTOR, 'span.JobSearchCard-primary-heading-days')
+            days = days_element.text
+            
+            avg_price_element = job_listing.find_element(By.CSS_SELECTOR, 'div.JobSearchCard-secondary-price')
+            # remove (Avg Bid) from the text
+            avg_price = avg_price_element.text[0:-9]
+            avg_price = avg_price.replace("$", "")
+            # Convert string to int and add dollar sign
+            avg_price = int(avg_price)
+            
+            bid_element = job_listing.find_element(By.CSS_SELECTOR, 'div.JobSearchCard-secondary-entry')
+            bid = bid_element.text
+            # Remove the " bids" at the end of the text
+            bid = int(bid[0:-5])
+            
+            element = job_listing.find_element(By.CSS_SELECTOR, 'a.JobSearchCard-primary-heading-link')
+            url = element.get_attribute('href')
+            
+            # Open a new tab, switch to it, and go to the url
+            driver.execute_script("window.open('');")
+            driver.switch_to.window(driver.window_handles[1])
+            driver.get(url) 
+               
+            try:
+                # wait for the QTextEdit element to be visible
+                # find the paragraph element using its class name
+                parent_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "PageProjectViewLogout-detail")))
+                # if the element not found, it will throw an exception
+                paragraph_element = parent_element.find_elements(By.TAG_NAME, "p")
+            except TimeoutException:
+                print("Job not found")
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                continue
+            # Extract the text from each p element
+            text_list = []
+            for p_element in paragraph_element:
+                text = p_element.text
+                text_list.append(text)
+            combined_text = "\n".join(text_list)
+        
+            budget_element = driver.find_element(By.XPATH, "//p[@class='PageProjectViewLogout-header-byLine']")
+            budget = budget_element.text
+            budget = budget[7:]
+            
+            # Save the data to the excel file
+            # project, days, avg_price, budget, bid, url, paragraph, 
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=FutureWarning)
+                
+                # Add the data to the dataframe
+                new_row = pd.DataFrame({'Project': project, 'Days': days, 'Avg Price ($)': avg_price, 'Budget': budget, 'Bid': bid, 'URL': url, 'Paragraph': combined_text}, index=[0])
+                df2 = pd.concat([df2, new_row], ignore_index=True)
+                
+            # Save the data to the excel file
+            df2.to_excel(writer, sheet_name="Job Listings", index=False)
+            # Close the tab
+            driver.close()
+            
+            # Switch back to the first tab
+            driver.switch_to.window(driver.window_handles[0])
+            
+            if i == len(job_listings) - 1:
+                print("Last element")
+        except:
+            continue
+        
 
-# Click hourly rate
+page = 1
 
-# project
-# budget
-# bids
-# average bid
-# content
+while True:
+    flag = False
+    # Scroll down to the button
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait for the job listings to be present
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.JobSearchCard-item'))
+    )
+    # Find all the job listings
+    job_listings = driver.find_elements(By.CSS_SELECTOR, '.JobSearchCard-item')
+    get_project()
+    try: 
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#last-entry'))
+        )
+        
+        last_entry_element = driver.find_element(By.CSS_SELECTOR, '#last-entry')
+        last_entry = last_entry_element.text
+    except:
+        pass
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#total-results-bottom'))
+        )
+        total_results_bottom_element = driver.find_element(By.CSS_SELECTOR, '#total-results-bottom')
+        total_results_bottom = total_results_bottom_element.text
+    except:
+        pass
+    if last_entry != total_results_bottom:
+        flag = True
+        page = page + 1
+        
+    if flag == True:
+        print(flag)
+        xpath = f"//li[@data-link='{page}']/a"
+        element = driver.find_element(By.XPATH, xpath)
+        element.click()
+        time.sleep(2)
+        continue
+    break
+
+writer.save()
+driver.close()
+driver.quit()
